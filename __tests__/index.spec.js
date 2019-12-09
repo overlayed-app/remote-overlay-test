3b39d9fe754845c7004e7cd6771934b863b4f494
bengreenier <ben@bengreenier.com>
2019-12-09 11:09:58 +0000

    fix tests

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -2,16 +2,20 @@ const React = require('react')
 const { render } = require('@testing-library/react')
 require('@testing-library/jest-dom/extend-expect')
 
-const App = require('../index')
+const AppAlloc = require('../index')
+
+const makeApp = () => {
+  return AppAlloc(React, React.version)
+}
 
 test('renders default greeting', () => {
-  const { getByText } = render(React.createElement(App))
+  const { getByText } = render(React.createElement(makeApp()))
   const textEl = getByText(/Hello world/i)
   expect(textEl).toBeInTheDocument()
 })
 
 test('renders custom greeting', () => {
-  const { getByText } = render(React.createElement(App, { name: 'user' }))
+  const { getByText } = render(React.createElement(makeApp(), { name: 'user' }))
   const textEl = getByText(/Hello user/i)
   expect(textEl).toBeInTheDocument()
 })
